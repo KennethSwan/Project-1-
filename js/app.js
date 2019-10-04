a10a9ba38a0088d30cba65c902b2c5b09be8eca1
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-10-04 15:56:59 -0500

    Added MVP Psuedocode

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// User sees screen with "Start Game" button,
+// click on that and get sent to next screen
+// user will see all of the levels of the game next
+
+// first question will appear as the levels go away.
+// Maybe use a timer to go to the question?
+// User will select answer they believe is correct,
+// Each question will need an if statement to remove 
+// 2 of the wrong answers due to the nature of the game and
+// the 50/50 lifeline.
+// "Phone a friend" and "Ask the audience" will have
+// buttons that work, but they will automatically 
+// select the correct answer.
+// Each lifeline can only be used once! 
+// Questions will become increasingly more difficult
+// Set timer for each question?
+
+
+// If user answers correctly, make button red
+// If user answers wrong, make button selected red
+// SHOW GRITTY IF INCORRECT
+// If correct answer is selected, proceed to show
+// user the next amount they will play for
+// give option to keep playing or quit the game.
+// If keep playing, proceed to next question
+// If quit the game, show another picture of GRITTY 
+// on the game over screen
+// User keeps going if they keep answering correctly 
+// and decide to keep playing
+// If get all questions right, show Congratulations
+// along with a DIFFERENT happier mascot
+
+// if loop will be needed for each question if any of 
+// the three incorrect answers are chosen
+// use dancing gritty gif on red background with 
+// GAME OVER and give option to play again from 
+// from the beginning.
+
+
